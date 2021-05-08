758b810008fb635acf98e6202fa23cf0832431b3
LonnonjamesD <LonnonjamesD@gmail.com>
2021-05-08 12:28:34 -0400

    Error Throwing

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,5 @@
-from enum import Enum
-
 from src.builtins import *
-from src.errorhandler import Error
+from src.errorhandler import Error, ErrorType
 
 #-----------------------------------------------------------------|
 #-                                                                |
@@ -10,7 +8,7 @@ from src.errorhandler import Error
 #-----------------------------------------------------------------|
 
 #* This checks the Lexed code and parses it and runs it
-def Parser(filename, text):
+def Parser(text):
 	iterate = 0
 	text = ConvertVars(text)
 	while iterate != len(text):
@@ -23,7 +21,7 @@ def Parser(filename, text):
 					elif text[iterate][0] == "outln":
 						outlnFUNC(text[iterate+1][0])
 				elif text[iterate+1][1] == "STRING" and text[iterate+1][0] == '"':
-					Error.throw(ErrorType.InvalidString, filename, 1, "Your Mom")
+					Error().throw(ErrorType.InvalidString, 1)
 			except:
 				pass
 			#_ Look for the word vent and make sure it has the type of FUNC
@@ -31,6 +29,7 @@ def Parser(filename, text):
 				break
 		iterate += 1
 
+#* Converts the var with a space to var with no space
 def ConvertVars(lexed):
 	storedvars = {}
 	iterate = 0
@@ -45,6 +44,3 @@ def ConvertVars(lexed):
 			lexed[iterate] = [var[0], var[1]]
 		iterate += 1
 	return lexed
-
-class ErrorType(Enum):
-    InvalidString = 0x00
