be10d52836967af91d0909979be2a6f1f3edccf6
LonnonjamesD <LonnonjamesD@gmail.com>
2021-05-08 16:16:34 -0400

    Tabs

--- src/errorhandler.py
+++ src/errorhandler.py
@@ -22,25 +22,25 @@ def GetFileName():
 
 #* Class containing ErrorType, and maybe file and line if needed
 class Error(): 
-    #* Initializer, takes ErrorType and optionally file and line
-    def __init__(self,err,line=None,file=None): 
-        #$ If err is of type ErrorType, set initializer values, otherwise, raise a TypeError.
-        if isinstance(err,ErrorType):
-            #_ Set fields equal to initializer values
-            self.err = err
-            self.file = file or GetFileName()
-            self.line = line
-        else:
-            raise TypeError("Error(err,file,line) -- type of err must be ErrorType enum!")
-    def __str__(self):
-        #$ If no file AND no line,
-        if self.file is None and self.line is None:
-            return f"Error:\n    {self.err}"
-        #$ Else if there is a file erroring but no specific line,
-        elif self.line is None:
-            return f"Error in file '{self.file}'\n    {self.err}"
-        #$ Otherwise (if there is a file and a line),
-        else:
-            return f"Error in file '{self.file}' on line {self.line}\n    {str(self.err)}"
-    def errout(self):
-        ctext(str(self), text="red", bg="black")
+	#* Initializer, takes ErrorType and optionally file and line
+	def __init__(self,err,line=None,file=None): 
+		#$ If err is of type ErrorType, set initializer values, otherwise, raise a TypeError.
+		if isinstance(err,ErrorType):
+			#_ Set fields equal to initializer values
+			self.err = err
+			self.file = file or GetFileName()
+			self.line = line
+		else:
+			raise TypeError("Error(err,file,line) -- type of err must be ErrorType enum!")
+	def __str__(self):
+		#$ If no file AND no line,
+		if self.file is None and self.line is None:
+			return f"Error:\n    {self.err}"
+		#$ Else if there is a file erroring but no specific line,
+		elif self.line is None:
+			return f"Error in file '{self.file}'\n    {self.err}"
+		#$ Otherwise (if there is a file and a line),
+		else:
+			return f"Error in file '{self.file}' on line {self.line}\n    {str(self.err)}"
+	def errout(self):
+		ctext(str(self), text="red", bg="black")
--- src/errortype.py
+++ src/errortype.py
@@ -8,19 +8,19 @@ from enum import Enum
 
 #* Parent Class -- each new enum member has its value incremented
 class AutoIncrementEnum(Enum): 
-    #* new(class,argument) -- cls is the class inheriting, arg is the value passed into the unary tuple
-    def __new__(cls,arg):
-        #_ Value = However many enum members there are currently (INVALID_STRING would be 0 because there are none, then ADDITIONAL_INFO 1, etc.)
-        value = len(cls.__members__)
-        obj = object.__new__(cls)
-        obj._value = value
-        #_ Value2 is set to arg.
-        obj.value2 = arg
-        return obj
+	#* new(class,argument) -- cls is the class inheriting, arg is the value passed into the unary tuple
+	def __new__(cls,arg):
+		#_ Value = However many enum members there are currently (INVALID_STRING would be 0 because there are none, then ADDITIONAL_INFO 1, etc.)
+		value = len(cls.__members__)
+		obj = object.__new__(cls)
+		obj._value = value
+		#_ Value2 is set to arg.
+		obj.value2 = arg
+		return obj
 #* Parent class is AutoIncrementEnum which means when __new__ is ran here (ie. by each enum member), it will also carry up to the parent
 class ErrorType(AutoIncrementEnum):
-    def __str__(self):
-        return "Error code " + str(self._value) + ": '" + self.value2 + "'"
+	def __str__(self):
+		return "Error code " + str(self._value) + ": '" + self.value2 + "'"
 
-    INVALID_STRING = ("Invalid String Format")
-    ADDITIONAL_INFO = ("Additional Info")
+	INVALID_STRING = ("Invalid String Format")
+	ADDITIONAL_INFO = ("Additional Info")
