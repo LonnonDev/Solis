0648282fa6466b66dc47b8f602af514415884a7d
LonnonjamesD <LonnonjamesD@gmail.com>
2021-05-08 12:10:30 -0400

    Errorhandling

--- src/errorhandler.py
+++ src/errorhandler.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import sys
 
 #-----------------------------------------------------------------|
 #-                                                                |
@@ -5,15 +7,39 @@
 #-                                                                |
 #-----------------------------------------------------------------|
 
+def GetFileName():
+	file = ""
+	try:
+		file = str(sys.argv[1])
+		f = (open(file, "r"))
+	except:
+		file = "text.sol"
+		f = open(file, "r")
+	return file
+
 class Error():
-    def throw(self, errortype, file, line, message: str = ""):
-        if errortype == 0x00:
-            if message == "":
-                message = "No Info Provided"
-            Error.create(file, message, line)
-    
-    def create(self, file, message, line):
-        return f"""
-In {file} at line {line},
-    {message}
-"""
+	def __init__(self, file):
+		self.file = GetFileName()
+
+	def throw(self, errortype, line, message: str = ""):
+		print(errortype)
+		if errortype.value == ErrorType.InvalidString.value:
+			if message == "":
+				message = "No Info Provided"
+			self.Error.create(message, line)
+	
+	def create(self, message: str = None, line: str = None):
+		if message and line:
+			return f"In {self.file} at line {line}\n\t{message}"
+		elif message and not line:
+			return f"In {self.file}\n\t{message}"
+		elif not message and line:
+			return f"In {self.file} at line {line}"
+		else:
+			return f"In {self.file}"
+
+class ErrorType(Enum):
+	InvalidString = 0
+
+	def value(self):
+		return self.value
